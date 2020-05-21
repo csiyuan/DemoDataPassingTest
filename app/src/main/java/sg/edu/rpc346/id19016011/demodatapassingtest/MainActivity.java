d3ead1f861d94ac58303cb0236b5f20d1196498a
CHENG SI YUAN <19016011@myrp.edu.sg>
2020-05-21 14:55:23 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rpc346/id19016011/demodatapassingtest/MainActivity.java
@@ -0,0 +1,53 @@
+package sg.edu.rpc346.id19016011.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPassInteger, btnPassChar;
+    TextView tvDouble;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+        btnPassChar = findViewById(R.id.buttonPassChar);
+        tvDouble = findViewById(R.id.textViewDouble);
+
+
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+        btnPassChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value", "a");
+                startActivity(intent);
+            }
+        });
+
+        tvDouble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FourthActivity.class);
+                intent.putExtra("value", "hi");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/sg/edu/rpc346/id19016011/demodatapassingtest/ThirdActivity.java
@@ -0,0 +1,23 @@
+package sg.edu.rpc346.id19016011.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ThirdActivity extends AppCompatActivity {
+    TextView tvAnswer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        tvAnswer=findViewById(R.id.textView);
+        Intent intentReceived = getIntent();
+        char value = intentReceived.getCharExtra("value", 'a');
+        tvAnswer.setText("Character value received is: " + value);
+    }
+}
